49a035105eaa10d8b0a27a306971fa33d2acb7de
David Emery <Emery2100@yahoo.com>
2026-04-20 19:52:17 -0700

    Initial Commit

---
+++ PhotoLogger/PhotoLoggerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoLoggerApp.swift
+//  PhotoLogger
+//
+//  Created by David Emery on 4/20/26.
+//
+
+import SwiftUI
+
+@main
+struct PhotoLoggerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
